509726ac924536f6cd0322fb6bc21072e8639b8d
ivanleopoldo <ivanleopoldoc@gmail.com>
2025-04-21 19:10:18 +0800

    feat: connects to supabase

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,20 @@
 import { View, Text } from 'react-native';
+import { supabase } from '@/lib/db/supabase';
+import React from 'react';
 
 export default function Root() {
+  React.useEffect(() => {
+    getTodos();
+  }, []);
+
+  const getTodos = async () => {
+    const { data, error } = await supabase.from('todos').select('*');
+
+    if (data) {
+      console.log(data);
+    }
+  };
+
   return (
     <View>
       <Text>Root</Text>
